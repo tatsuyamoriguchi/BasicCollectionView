fb54e0f49cbb5dcb3623c8ad2fe94c9d8edd54f1
Tatsuya Moriguchi <tatsuya.moriguchi@icloud.com>
2023-10-25 09:58:36 -0700

    Simple Compositional Layout up to p.568

---
+++ BasicCollectionView/BasicCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  BasicCollectionViewCell.swift
+//  BasicCollectionView
+//
+//  Created by Tatsuya Moriguchi on 10/25/23.
+//
+
+import UIKit
+
+class BasicCollectionViewCell: UICollectionViewCell {
+    @IBOutlet var stateLabel: UILabel!
+    
+}
